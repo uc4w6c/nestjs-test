fdde443a2a8471e5ee12f87314ef53e8b368e820
Yuta <uc4w6c@bma.biglobe.ne.jp>
2020-01-26 12:28:13 +0900

    interceptorを試す

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { LoggingInterceptor } from './LoggingInterceptor'
 
 @Module({
   imports: [CatsModule],
